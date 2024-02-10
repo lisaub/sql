818dea7fe4cf1c0bece6a7aa4bb6446351ee8b68
lisaub <57632065+lisaub@users.noreply.github.com>
2024-02-10 23:55:09 +0100

    Create backup.sql

---
+++ backup.sql
@@ -0,0 +1,22 @@
+mysqldump --no-create-info company_db > company_db_backup.sql
+-- mysqldump -u [username] -p[password] --no-create-info company_db > company_db_backup.sql
+
+-- SQL Data Dump
+-- Database: company_db
+-- Tables included in the data backup:
+-- departments
+-- performance_evaluations
+-- vacation_history
+-- positions
+-- employees
+-- capacitacion
+-- beneficio
+-- incidente_laboral
+-- reclutamiento
+-- viaje_laboral
+-- employees_log
+-- performance_evaluations_log
+-- Generated on: 10/02/24
+
+
+
